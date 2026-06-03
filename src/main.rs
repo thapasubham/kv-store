1ad8f643ca664d3c54f009c9a406ab13284e08b5
Subham Thapa <subhamits4.2@gmail.com>
2026-06-03 13:57:10 +0545

    simple set command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::str::Bytes;
 
+let mut db: HashMap<String, String> = HashMap::new();
 fn main() {
     let addr = "127.0.0.1:5000";
 
@@ -34,7 +37,10 @@ fn handle_client(mut stream: TcpStream) {
         }
         Ok(n) => {
             println!("Received {} bytes", n);
-            println!("{}", String::from_utf8_lossy(&buf[..n]));
+            let input = String::from_utf8_lossy(&buf[..n]);
+            let response = handle_command(&input, db);
+
+            let _ = stream.write_all(response.as_bytes());
         }
         Err(err) => {
             eprintln!("Read failed: {}", err);
@@ -45,4 +51,21 @@ fn handle_client(mut stream: TcpStream) {
     if let Err(err) = writeln!(stream, "hello") {
         eprintln!("Write failed: {}", err);
     }
+}
+
+
+fn handle_command(
+    input: &str,
+    db: &mut HashMap<String, String>,
+) -> String {
+    let parts: Vec<&str> =
+        input.trim().split_whitespace().collect();
+
+    if parts.len() == 3 && parts[0].eq_ignore_ascii_case("SET") {
+        db.insert(parts[1].to_string(), parts[2].to_string());
+
+        return "OK\n".to_string();
+    }
+
+    "ERR\n".to_string()
 }
