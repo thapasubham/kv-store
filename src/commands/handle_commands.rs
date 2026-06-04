e131f08924a2e2213a3bc28089fa5ee7395c8e6d
Subham Thapa <subhamits4.2@gmail.com>
2026-06-04 18:38:40 +0545

    handle other text after send index

--- src/commands/handle_commands.rs
+++ src/commands/handle_commands.rs
@@ -5,8 +5,9 @@ use super::{get_value, set_value};
 pub fn handle_command(input: &str, db: &mut Database) -> String {
     let parts: Vec<&str> = input.trim().split_whitespace().collect();
 
-    if parts.len() == 3 && parts[0].eq_ignore_ascii_case("SET") {
-        return set_value::handle(db, parts[1], parts[2]);
+    if parts.len() >= 3 && parts[0].eq_ignore_ascii_case("SET") {
+        let value = parts[2..].join(" ");
+        return set_value::handle(db, parts[1], &value);
     }
 
     if parts.len() == 2 && parts[0].eq_ignore_ascii_case("GET") {
